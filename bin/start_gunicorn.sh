f26ab769d62c2bb53f6d740a074c447cfbce61a4
rikamalov <74410332+rikamalov@users.noreply.github.com>
2020-12-01 00:44:05 +0300

    Update start_gunicorn.sh

--- bin/start_gunicorn.sh
+++ bin/start_gunicorn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source dbms_template_path/venv/bin/activate
+source dbms_template_path/env/bin/activate
 exec gunicorn -c "dbms_template_path/djangowebsite/gunicorn_config.py" config.wsgi
