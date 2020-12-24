a92de5e9171b942c9907b539b604d864225610bc
Rustam Kamalov <74410332+rikamalov@users.noreply.github.com>
2020-12-24 19:56:47 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ which python3
 
 read -p "Укажите путь к Python: " base_python_interpreter
 read -p "Ваше доменное имя, без http (for example, google.com): " project_domain
-read -p "IP адрес сервера, без порта (for example, 155.155.255.50): " project_ip
 read -p "Укажите количество workers, это цифра равна количество ядер умноженные на 2 + 1 (прим: 1 ядро - 3 workers): " project_workers
 `$base_python_interpreter -m venv env`
 source env/bin/activate
@@ -22,6 +21,9 @@ mv config djangowebsite
 sudo sed -i "s~dbms_template_path~$project_path~g" gunicorn_config.py
 sudo sed -i "s~dbms_template_user~$user_linux~g" gunicorn_config.py
 sudo sed -i "s~dbms_template_workers~$project_workers~g" gunicorn_config.py
+
+sudo sed -i "s~dbms_template_path~$project_path~g" default
+sudo sed -i "s~dbms_template_web_adress~$project_domain~g" default
 
 sudo sed -i "s~dbms_template_path~$project_path~g" bin/start_gunicorn.sh
 sudo chmod +x bin/start_gunicorn.sh
